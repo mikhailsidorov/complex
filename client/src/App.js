ec7357ff26bfff81ec16dec6605d5351a5d02a41
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2019-03-13 03:01:55 +0300

    Add client (#3)

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import OtherPage from './OtherPage'
+import Fib from './Fib'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </a>
+
+            <Link to="/home">Home</Link>
+            <Link to="/otherpage">Other page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
